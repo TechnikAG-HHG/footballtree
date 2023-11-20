151c3d870a4ef2b2e25e6e0651657d800c544f58
EinEisbxr <flixe0001@gmail.com>
2023-11-20 17:22:33 +0100

    made plan website better

--- code/static/websiteplan.js
+++ code/static/websiteplan.js
@@ -1,4 +1,8 @@
 var matchCount = 0; // Global variable to keep track of the total number of matches
+var timeInterval = 8;
+
+var startTime = new Date(); // Set the start time
+startTime.setHours(10, 0, 0, 0); // Set the start time to 10:00
 
 function calculateMatches() {
     matchCount = 0; // Reset matchCount to 0
@@ -20,12 +24,14 @@ function calculateMatches() {
 
     var matches = interleaveMatches(matches1, matches2);
 
+    matchCount = 0; // Reset matchCount to 0
+
     matches = matches.map(function(match) {
         matchCount++; // Increment the match count
-        match.number = "Spiel " + (matchCount - 30);
+        match.number = "Spiel " + (matchCount);
         return match;
     });
-
+  
     generateTableGroup(matches);
 }
 
@@ -104,10 +110,18 @@ function generateTableGroup(matches) {
         table.className = "tableGroup";
 
         var headerRow = thead.insertRow(0);
-        var headerCellGroup = headerRow.insertCell(0);
+        var headerCellTime = headerRow.insertCell(0);
+        headerCellTime.textContent = "Zeit";
+        headerCellTime.className = "headerCell";
+
+        var headerCellGroup = headerRow.insertCell(1);
         headerCellGroup.textContent = "Spiele";
         headerCellGroup.className = "headerCell";
         headerCellGroup.colSpan = 4;
+
+        var headerCellT = headerRow.insertCell(2);
+        headerCellT.textContent = "Tore";
+        headerCellT.className = "headerCell";
 
         table.appendChild(thead);
         table.appendChild(tbody);
@@ -118,53 +132,28 @@ function generateTableGroup(matches) {
     matches.forEach(function(match) {
         var row = tbody.insertRow();
 
-        var cellMatchNumber = row.insertCell(0);
+        var cellTime = row.insertCell(0);
+        var matchNumber = parseInt(match.number.split(" ")[1]);
+        var matchTime = new Date(startTime.getTime() + matchNumber * timeInterval * 60000);
+        cellTime.textContent = formatTime(matchTime);
+
+        console.log(match.number);
+
+        var cellMatchNumber = row.insertCell(1);
         cellMatchNumber.textContent = match.number;
 
-        var cellGroup = row.insertCell(1);
+        var cellGroup = row.insertCell(2);
         cellGroup.textContent = match.group;
 
-        var cellFirstTeam = row.insertCell(2);
+        var cellFirstTeam = row.insertCell(3);
         cellFirstTeam.textContent = match.teams[0];
 
-        var cellSecondTeam = row.insertCell(3);
+        var cellSecondTeam = row.insertCell(4);
         cellSecondTeam.textContent = match.teams[1];
+
+        var cellT = row.insertCell(5);
+        cellT.textContent = ":";
     });
-}
-
-
-function createTableForGroup(matches, tablesContainer) {
-    var table = document.createElement("table");
-    var thead = document.createElement("thead");
-    var tbody = document.createElement("tbody");
-
-    table.className = "tableGroup";
-
-    var headerRow = thead.insertRow(0);
-    var headerCellGroup = headerRow.insertCell(0);
-    headerCellGroup.textContent = "Spiele";
-    headerCellGroup.className = "headerCell";
-    headerCellGroup.colSpan = 4;
-
-    matches.forEach(function(match) {
-        var row = tbody.insertRow();
-
-        var cellMatchNumber = row.insertCell(0);
-        cellMatchNumber.textContent = match.number;
-
-        var cellGroup = row.insertCell(1);
-        cellGroup.textContent = match.group;
-
-        var cellFirstTeam = row.insertCell(2);
-        cellFirstTeam.textContent = match.teams[0];
-
-        var cellSecondTeam = row.insertCell(3);
-        cellSecondTeam.textContent = match.teams[1];
-    });
-
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    tablesContainer.appendChild(table);
 }
 
 
@@ -193,6 +182,14 @@ function updateData() {
         calculateMatches();
     }, 500);
 }
+
+
+function formatTime(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    return (hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' Uhr';
+}
+
 
 calculateMatches();
 // call updateData() every 5 seconds
