86d8e05bfad75a4f6a8c9b9f4bc217b5d7fe0635
EinEisbxr <flixe0001@gmail.com>
2024-03-02 10:14:23 +0100

    added base url and mirrored teams again

--- code/static/websitetv.js
+++ code/static/websitetv.js
@@ -1,6 +1,6 @@
 function getTeamData() {
-    var teamName2 = data["Matches"][data["activeMatchNumber"]][0];
-    var teamName1 = data["Matches"][data["activeMatchNumber"]][1];
+    var teamName1 = data["Matches"][data["activeMatchNumber"]][0];
+    var teamName2 = data["Matches"][data["activeMatchNumber"]][1];
 
     var teamName1Element = document.getElementById("nextTeam1");
     var teamName2Element = document.getElementById("nextTeam2");
@@ -10,8 +10,8 @@ function getTeamData() {
 
     console.log(teamName1, teamName2);
 
-    var score2 = data["Matches"][data["activeMatchNumber"]][2];
-    var score1 = data["Matches"][data["activeMatchNumber"]][3];
+    var score1 = data["Matches"][data["activeMatchNumber"]][2];
+    var score2 = data["Matches"][data["activeMatchNumber"]][3];
 
     var scoreElement = document.getElementById("scoreTeams");
     scoreElement.textContent = `${score1} : ${score2}`;
