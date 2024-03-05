70e98fe8c56ff7cf57c85423d12e516b95dd9bda
EinEisbxr <flixe0001@gmail.com>
2024-03-05 17:30:58 +0100

    redirect uri auth hotfix

--- code/main.py
+++ code/main.py
@@ -3384,7 +3384,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://technikag.serveo.net/callback"
+    redirect_uri="https://technikag.serveo.net/callback"
 )
 
 
