55bc75bfad911dd1218c3b455379fedfe06d9b11
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-03-10 21:13:20 +0100

    Update websitetipping.js (#348)
    * Update websitetipping.js
    
    * hotfix

--- code/static/websitetipping.js
+++ code/static/websitetipping.js
@@ -201,7 +201,7 @@ function generateDropdownData() {
         }
     }
 
-    if (!(data["activeMatchNumber"] < -4)) {
+    if (!(data["activeMatchNumber"] < -4) && data["activeMatchNumber"] != -1) {
         var groupDivider = document.createElement("option");
         groupDivider.disabled = true;
         groupDivider.textContent = "";
@@ -400,6 +400,9 @@ function handleSubmit(event) {
     if (match.split(".")[1]) {
         if (match.split(".")[1].startsWith(" Halbfinale")) {
             matchNumber = parseInt(match.split(".")[0]) * -1 - 1;
+        } else if (match.split(".")[1].startsWith(" K")) {
+            matchData = data["KOMatches"][matchNumber];
+            matchNumber = (parseInt(match.split(".")[0]) + 99) * -1;
         } else {
             matchNumber = match.split(".")[0] - 1;
         }
